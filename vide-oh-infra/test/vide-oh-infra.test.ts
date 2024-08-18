87f2c420287525b26585bcdce51e45736f3cfb36
milomilo33 <milovanovicm309@gmail.com>
2024-08-19 01:25:33 +0200

    Init infra CDK app

---
+++ vide-oh-infra/test/vide-oh-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VideOhInfra from '../lib/vide-oh-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vide-oh-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VideOhInfra.VideOhInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
