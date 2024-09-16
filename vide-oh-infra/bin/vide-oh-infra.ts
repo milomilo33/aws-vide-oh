5b8b91cc7e8c08e5cca7fc56f3cf89dc07ec063a
milomilo33 <milovanovicm309@gmail.com>
2024-09-16 07:03:34 +0200

    Initial working CDK vue app deployment

--- vide-oh-infra/bin/vide-oh-infra.ts
+++ vide-oh-infra/bin/vide-oh-infra.ts
@@ -3,8 +3,14 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { VPCStack } from '../lib/vpc-stack';
 import { RDSStack } from '../lib/rds-stack';
+import { VueAppStack } from '../lib/vue-app-stack';
 
 const app = new cdk.App();
 const vpcStack = new VPCStack(app, `videoh-vpc`, {});
 const rdsStack = new RDSStack(app, `videoh-db`, { vpc: vpcStack.vpc });
 rdsStack.addDependency(vpcStack);
+const vueAppStack = new VueAppStack(app, 'videoh-vue-appx', {
+    env: {
+        region: 'eu-central-1',
+    },
+});
---
+++ vide-oh-infra/lib/vue-app-stack.ts
@@ -0,0 +1,148 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { BlockPublicAccess, Bucket } from 'aws-cdk-lib/aws-s3';
+import { BucketDeployment, Source } from 'aws-cdk-lib/aws-s3-deployment';
+import { Distribution } from 'aws-cdk-lib/aws-cloudfront';
+import { S3Origin } from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Construct } from 'constructs';
+import { CfnBucketPolicy } from 'aws-cdk-lib/aws-s3';
+import path = require('path');
+
+export class VueAppStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const websiteBucket = new Bucket(this, 'VueCdkDemoSiteBucket', {
+      websiteIndexDocument: 'index.html',
+      blockPublicAccess: BlockPublicAccess.BLOCK_ACLS, // Allow public access via policies
+    });
+
+    // Add a bucket policy to allow public read access
+    new CfnBucketPolicy(this, 'BucketPolicy', {
+      bucket: websiteBucket.bucketName,
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Effect: 'Allow',
+            Principal: '*',
+            Action: 's3:GetObject',
+            Resource: `${websiteBucket.bucketArn}/*`,
+          },
+        ],
+      },
+    });
+
+    const webDistribution = new Distribution(this, 'VueCdkDistribution', {
+      defaultBehavior: {
+        origin: new S3Origin(websiteBucket),
+        // You might need to add other properties here based on your requirements
+      },
+      errorResponses: [
+        {
+          httpStatus: 403,
+          responsePagePath: '/index.html',
+          responseHttpStatus: 200,
+        },
+        {
+          httpStatus: 404,
+          responsePagePath: '/index.html',
+          responseHttpStatus: 200,
+        },
+      ],
+    });
+    const SRC_PATH = path.join(__dirname, "../../vide-oh-fe/dist");
+    new BucketDeployment(this, 'VueCdkDemoSiteDeployment', {
+      sources: [Source.asset(SRC_PATH)],
+      destinationBucket: websiteBucket,
+      distribution: webDistribution,
+      distributionPaths: ['/*'],
+      prune: true
+    });
+  }
+}
+
+// import * as cdk from "aws-cdk-lib";
+// import {aws_cloudfront as cloudfront, aws_s3 as s3, aws_s3_deployment as s3Deployment} from "aws-cdk-lib";
+// import {Construct} from "constructs";
+// import * as path from "path";
+// import {spawnSync} from "child_process";
+
+// const SRC_PATH = path.join(__dirname, "../../vide-oh-fe");
+// const BUILD_CONFIG = "development";
+
+// export class VueAppStack extends cdk.Stack {
+//    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//        super(scope, id, props);
+
+//        const vueBucket = new s3.Bucket(this, "VueBucket", {
+//            bucketName: "video-app-fe-bucket",
+//            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+//            autoDeleteObjects: true,
+//            removalPolicy: cdk.RemovalPolicy.DESTROY,
+//        });
+
+//        const webDistribution = this.createCloudFrontDistribution(vueBucket);
+//        new cdk.CfnOutput(this, "VueAppDomainName", {
+//            value: webDistribution.distributionDomainName,
+//        });
+
+//        this.createDeployment(vueBucket, webDistribution);
+//    }
+
+//    private createCloudFrontDistribution(webAppBucket: s3.IBucket) {
+//        return new cloudfront.CloudFrontWebDistribution(
+//            this,
+//            "VueAppWebDistribution",
+//            {
+//                originConfigs: [
+//                    {
+//                        s3OriginSource: {
+//                            s3BucketSource: webAppBucket,
+//                            originAccessIdentity: new cloudfront.OriginAccessIdentity(
+//                                this,
+//                                "originAccessIdentityForVueBucket"
+//                            ),
+//                        },
+//                        behaviors: [{ isDefaultBehavior: true }],
+//                    },
+//                ],
+//                errorConfigurations: [
+//                    {
+//                        errorCode: 403,
+//                        errorCachingMinTtl: 60,
+//                        responseCode: 200,
+//                        responsePagePath: "/index.html",
+//                    },
+//                    {
+//                        errorCode: 404,
+//                        errorCachingMinTtl: 60,
+//                        responseCode: 200,
+//                        responsePagePath: "/index.html",
+//                    },
+//                ],
+//            }
+//        );
+//    }
+
+//    private createDeployment(webAppBucket: s3.IBucket, webDistribution: cloudfront.CloudFrontWebDistribution) {
+//     console.log("ello")   
+//     new s3Deployment.BucketDeployment(this, "VueAppDeployment", {
+//            destinationBucket: webAppBucket,
+//            sources: [s3Deployment.Source.asset(path.join(__dirname, "../../vide-oh-fe/dist"), {
+//                bundling: {
+//                    image: cdk.DockerImage.fromRegistry("public.ecr.aws/docker/library/node:lts"),
+//                    local: {
+//                        tryBundle(outputDir: string) {
+//                         console.log("hello")
+//                            spawnSync("npm ci", { cwd: SRC_PATH, stdio: "inherit", shell: true });
+//                            spawnSync(`npm run build -- -c ${BUILD_CONFIG} --output-path ${outputDir}`, { cwd: SRC_PATH, stdio: "inherit", shell: true });
+//                            return true;
+//                        },
+//                    },
+//                },
+//            })],
+//            distribution: webDistribution,
+//        });
+//    }
+// }
+
