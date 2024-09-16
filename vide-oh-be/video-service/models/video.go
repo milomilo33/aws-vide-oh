8c411c684dbca231cf2c5bb53e35f6d6abbf1af3
milomilo33 <milovanovicm309@gmail.com>
2024-09-16 05:43:32 +0200

    Finished all video service routes (not fully tested, esp search)

--- vide-oh-be/video-service/models/video.go
+++ vide-oh-be/video-service/models/video.go
@@ -30,3 +30,12 @@ type Video struct {
 	OwnerEmail  string `json:"ownerEmail" gorm:"not null"`
 	Reported    bool   `json:"reported" gorm:"default:false"`
 }
+
+type VideoSearchResultDTO struct {
+	Title        string `json:"title"`
+	Filename     string `json:"filename"`
+	Description  string `json:"description"`
+	OwnerEmail   string `json:"ownerEmail"`
+	Reported     bool   `json:"reported"`
+	ThumbnailURL string `json:"thumbnailUrl"`
+}
